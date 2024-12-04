4c4e9259b6708f3ebe429c4d9f79023ed93b688e
BugHunter-X <askzenai@gmail.com>
2024-12-04 07:12:05 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,5 @@
+std::vector<int> vec = {1, 2, 3, 4, 5};
+for (int i = 0; i < vec.size(); ++i) { // Corrected: Loop condition changed to i < vec.size()
+    std::cout << vec[i] << " ";
+}
+std::cout << std::endl; 
